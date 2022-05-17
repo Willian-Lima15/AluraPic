d22460729450ec5f7a0332386b018ed2b71a41d4
Willian Lima <william_slayer@hotmail.com>
2022-05-17 16:36:27 -0300

    Implementando a logica do grid

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PhotosModel } from 'src/app/shared/models/photos.model';
 
 @Component({
@@ -6,14 +6,25 @@ import { PhotosModel } from 'src/app/shared/models/photos.model';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
 @Input() photos: PhotosModel[] = [];
 rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.photos)
+
+    this.rows = this.groupColumns(this.photos);
   }
 
+  groupColumns(photos: PhotosModel[]) {
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+=3) {
+      newRows.push(photos.slice(index, index + 3))
+    }
+    return newRows;
+  }
 }
