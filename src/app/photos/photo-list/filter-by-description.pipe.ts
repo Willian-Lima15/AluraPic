4aef39369cdb5f34aeb3410355b39284c2bae176
Willian Lima <william_slayer@hotmail.com>
2022-05-19 17:56:40 -0300

    Finalizado pipe

--- src/app/photos/photo-list/filter-by-description.pipe.ts
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { PhotosModel } from 'src/app/shared/models/photos.model';
 
 @Pipe({
   name: 'filterByDescription'
 })
 export class FilterByDescriptionPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(photos: PhotosModel[], descriptionQuery: string) {
+    descriptionQuery = descriptionQuery
+    .trim()
+    .toLowerCase();
+
+    if(descriptionQuery){
+      return photos.filter(photo =>
+         photo.description.toLowerCase().includes(descriptionQuery))
+    } else {
+      return photos;
+    }
   }
 
 }
