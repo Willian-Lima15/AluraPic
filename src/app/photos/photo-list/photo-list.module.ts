fe6ba85687cac22301645acf9f771f956c6642f2
Willian Lima <william_slayer@hotmail.com>
2022-05-21 20:51:04 -0300

    adicionando icones

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoListComponent } from './photo-list.component';
-import { PhotoComponent } from '../photo/photo.component';
+
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+import { PhotosComponent } from './photos/photos.component';
+import { PhotoModule } from '../photo/photo/photo.module';
 
 
 
 @NgModule({
   declarations: [
     PhotoListComponent,
-    PhotoComponent,
+    PhotosComponent,
     LoadButtonComponent,
     FilterByDescriptionPipe
   ],
   imports: [
     CommonModule,
+    PhotoModule
   ]
 })
 export class PhotoListModule { }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PhotoFormModule } from "./photo-form/photo-form/photo-form.module";
 
-import { PhotoListModule } from "./photo-list/photo-list.module.module";
+import { PhotoListModule } from "./photo-list/photo-list.module";
+import { PhotoModule } from "./photo/photo/photo.module";
+
 
 
 @NgModule({
   declarations: [],
   imports: [
-    PhotosModule,
+    PhotoModule,
     PhotoFormModule,
     PhotoListModule,
     CommonModule
