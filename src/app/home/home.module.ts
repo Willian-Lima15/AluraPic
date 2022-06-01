88f6b197121a682aaf98ac06d7bd96c01a2eb2da
Willian Lima <william_slayer@hotmail.com>
2022-06-01 14:47:02 -0300

    Redirecionamento pós login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,14 @@ import { SignInComponent } from "./signin/signin.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { VmessageModule } from "../shared/components/vmessage/vmessage.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [ SignInComponent ],
   imports: [ CommonModule,
      ReactiveFormsModule,
-    VmessageModule ]
+    VmessageModule,
+    RouterModule
+    ]
 })
 export class HomeModule {}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import { AuthService } from "src/app/core/auth.service";
 
 @Component({
@@ -11,7 +12,8 @@ export class SignInComponent implements OnInit{
 
   constructor(
     private formBuilder: FormBuilder,
-    private autheService: AuthService
+    private autheService: AuthService,
+    private router: Router
     ) {}
 
   ngOnInit(): void {
@@ -28,6 +30,12 @@ export class SignInComponent implements OnInit{
     const userName = this.loginForm.get('userName').value
     const password = this.loginForm.get('password').value
 
-    this.autheService.authenticate(userName, password).subscribe()
+    this.autheService.authenticate(userName, password)
+    .subscribe(() => this.router.navigate(['user', userName]),
+    err => {
+      console.log(err);
+      this.loginForm.reset();
+      alert('Invalid user name is password')
+    })
   }
 }
