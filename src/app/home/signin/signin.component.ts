2b0781fae6194acf0dfa40da0e81c2bf205f356c
Willian Lima <william_slayer@hotmail.com>
2022-06-01 14:23:42 -0300

    Enviando credenciais para a API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth.service";
 
 @Component({
   templateUrl: "./signin.component.html"
@@ -8,7 +9,10 @@ export class SignInComponent implements OnInit{
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private autheService: AuthService
+    ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -16,6 +20,14 @@ export class SignInComponent implements OnInit{
        Validators.required],
       password: ['',
        Validators.required]
-    })
+    });
+  }
+
+  login() {
+
+    const userName = this.loginForm.get('userName').value
+    const password = this.loginForm.get('password').value
+
+    this.autheService.authenticate(userName, password).subscribe()
   }
 }
