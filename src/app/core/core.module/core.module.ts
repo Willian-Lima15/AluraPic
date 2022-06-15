8572d5f9aa42bcd9e7e1c430e96061a0088c299f
Willian Lima <william_slayer@hotmail.com>
2022-06-15 09:27:30 -0300

    interceptores

--- src/app/core/core.module/core.module.ts
+++ src/app/core/core.module/core.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from "@angular/common";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { HeaderComponent } from "../header/header.component";
+import { RequestInterceptor } from "../request.interceptor";
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }]
 })
 export class CoreModule {
 
