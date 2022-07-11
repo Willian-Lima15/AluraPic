8d94bed71a6393c02f116dea9e20d41e77587fdc
Willian Lima <william_slayer@hotmail.com>
2022-07-11 14:13:04 -0300

    A camada de serviço do component

---
+++ src/app/core/alert.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { AlertModel, AlertType } from "../shared/interfaces/alertModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  alertSubject: Subject<AlertModel>;
+
+  sucess(message: string) {
+    this.alert(AlertType.SUCCESS, message);
+  }
+
+  warning(message: string) {
+    this.alert(AlertType.SUCCESS, message);
+  }
+
+  danger(message: string) {
+    this.alert(AlertType.DANGER, message);
+  }
+
+  info(message: string) {
+    this.alert(AlertType.INFOR, message)
+  }
+
+  private alert(alertType: AlertType, message: string) {
+
+    this.alertSubject.next(new AlertModel(alertType, message))
+  }
+
+  getAlert() {
+    return this.alertSubject.asObservable();
+  }
+}
