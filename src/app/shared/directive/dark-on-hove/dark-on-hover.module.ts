1a9f1c0b4da2a80b93e6669884b16eee38cf870a
Willian Lima <william_slayer@hotmail.com>
2022-09-03 18:55:28 -0300

    .

--- src/app/shared/directive/dark-on-hove/dark-on-hover.module.ts
+++ src/app/shared/directive/dark-on-hove/dark-on-hover.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from "@angular/core";
 import { DarkOnHoverDirective } from "./dark-on-hover.directive";
-import { ShowIfLoggedDirective } from '.c:/Users/Willian Lima/Documents/Alura/Angular/AluraPic/src/app/shared/directive/show-if-logged.directive';
 
 @NgModule({
   declarations: [	 DarkOnHoverDirective,
-      ShowIfLoggedDirective
    ],
   exports: [ DarkOnHoverDirective]
 })
